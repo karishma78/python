93a88c0799253f64902d5ad5bdb844c53ee49e22
karishma78 <60160186+karishma78@users.noreply.github.com>
2020-06-09 14:45:38 -0500

    Add files via upload

---
+++ scripts/part4.py
@@ -0,0 +1,9 @@
+differences=str("Essentially, in Python 3, the print statement has been replaced with a print () function\n"
+                "Python 2 has ASCII str() types, separate unicode(), but no byte type.Now, in Python 3, we finally have Unicode"
+                " (utf-8) strings, \nand 2 byte classes: byte and bytearrays.differences Essentially\n"
+                " in Python 3, the print statement has been replaced with a print () function\n"
+                "In Python 3, the range() was implemented like the xrange() function so that a dedicated "
+                "xrange() function does not exist anymore \n(xrange() raises a NameError in Python 3\n")
+
+
+print(differences)
